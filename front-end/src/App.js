a930af7e28be67cbf7ca896d5500d0bc9452335b
Nick <azwar.hanik@gmail.com>
2022-04-10 17:14:46 +0800

    Create-react-app

---
+++ front-end/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div>
+      <header >
+        <a href="/">amazon</a>
+      </header>
+      <main>
+        list product
+      </main>
+    </div>
+  );
+}
+
+export default App;
