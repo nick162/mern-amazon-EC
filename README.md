6a12a096bc40c04a8ac251960a792b5a23d7037d
nick162 <azwar.hanik@gmail.com>
2022-07-16 17:33:08 +0700

    Update README.md, data.js, and 17 more files...

--- README.md
+++ README.md
@@ -14,3 +14,53 @@
     1. npm i react-router-dom
     2. create route for home screen
     3. create route for product screen
+7. Create Node.JS Server
+    1. run npm init in root folder
+    2. Update package.json set type: module
+    3. Add .js imports
+    4. npm install express
+    5. create server js 
+    6. add start comment as node backend/server.js
+    7. require express 
+    8. create route for / return backend ready
+    9. move product.js from front-end to backend
+    10. create route for product fro /api/products
+    11. return products
+    12. run npm start
+8. Fetch Product From Backend
+    1. set proxy in package.json
+    2. install axios 
+    3. use state hook
+    4. use effect hook 
+    5. use reducer hook
+9. Manage State By Reducer Hook
+    1. define reducer
+    2. update fetch data
+    3. get state from useReducer
+10. Add bootstrap UI Framework
+    1. npm install react-bootstrap bootstrap 
+    2. update App.js
+11. Create Product and Rating Component
+    1. create Rating Component
+    2. Create Product Component
+    3. Use rating component in Product Component
+12. Create Product Detail Screen
+    1. fecth product from backend
+    2. Create 3 coloumns for image, info and action
+13. Create Loading and Message Component
+    1. create loading component
+    2. use spinner component
+    3. create message component
+    4. create utils.js to define getError function
+14. Implement Add to Cart
+    1. Create React Context
+    2. Define Reducer
+    3. Create Store Provider
+    4. Implement Add to Cart Button Click handler
+15. complete Add to Cart
+    1. check exist item in the cart
+    2. check count in stock backend
+16. Creat Cart Screen
+    1. create 2 coloumns
+    2. display items list
+    3. create action coloumns
--- front-end/src/screens/HomeScreen.js
+++ front-end/src/screens/HomeScreen.js
@@ -1,28 +1,71 @@
-import { Link } from "react-router-dom";
-import data from "../data";
+// import data from "../data";
+import axios from "axios";
+import { useEffect, useReducer } from "react";
+import logger from "use-reducer-logger";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Product from "../component/Product.js";
+import { Helmet } from "react-helmet-async";
+import LoadingBox from "../component/LoadingBox";
+import MessageBox from "../component/MessageBox.js";
 
-function HomeScreen(){
-    return (
-        <div>
-            <h1>Feature Products</h1>
-            <div className="products">
-                {data.Products.map((product) => (
-                <div className="product" key={product.slug}>
-                    <Link to={`/product/${product.slug}`}>
-                        <img src={product.image} alt={product.name} />
-                    </Link>
-                    <div className="product-info">
-                    <Link to={`/product/${product.slug}`}>
-                        <p>{product.name}</p>
-                    </Link>
-                    <p><strong>$</strong>{product.price}</p>
-                    <button>Add to cart</button>
-                    </div>
-                </div>
-                ))}
-            </div>
-          </div>
-    )
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+function HomeScreen() {
+  const [{ products, loading, error }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: "",
+  });
+  // const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+      // setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Helmet>
+        <title>Amazona</title>
+      </Helmet>
+      <h1>Feature Products</h1>
+      <div className="products">
+        {loading ? (
+          <LoadingBox />
+        ) : error ? (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
